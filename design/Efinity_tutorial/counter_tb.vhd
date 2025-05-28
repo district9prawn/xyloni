fb0233ee607a422e9c53d0b7be634a436a4cdbb2
district9prawn <50253224+district9prawn@users.noreply.github.com>
2025-05-28 22:38:40 +1000

    GHDL simulation of Xyloni counter
    - Code formatting
    - Library change to numeric.std

--- design/Efinity_tutorial/counter_tb.vhd
+++ design/Efinity_tutorial/counter_tb.vhd
@@ -1,26 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_unsigned.ALL;
+entity counter_tb is
+end counter_tb;
 
-ENTITY testbench IS
-END testbench;
+architecture behavior of counter_tb is 
 
-ARCHITECTURE behavior OF testbench IS 
-
-	COMPONENT counter
-		port ( clk 			: in std_logic;								-- clock input. Could be from internal osc (T4/T8) or from the external 33.3MHz clock use GPIOl_20
-				 setn 		: in std_logic;								--  Set signal, low active; sett all outputs to '1' (LED are low active, means all LEDs msut be OFF) GPIOL_12
-				 stopn 		: in std_logic;								-- Stop signal, low active Stop counting GPIOL_13
-				 Dataout 	: out std_logic_vector ( 3 downto 0)); 		-- Output data connected to the LEDs (low active); GPIOL_21,GPIOL_18,GPIOL_16,GPIOL_09,GPIOL_03
-	END COMPONENT;
+	component counter
+		port ( 
+			clk 		: in std_logic;								-- clock input. Could be from internal osc (T4/T8) or from the external 33.3MHz clock use GPIOl_20
+			setn 		: in std_logic;								--  Set signal, low active; sett all outputs to '1' (LED are low active, means all LEDs msut be OFF) GPIOL_12
+			stopn 		: in std_logic;								-- Stop signal, low active Stop counting GPIOL_13
+			Dataout 	: out std_logic_vector ( 3 downto 0)); 		-- Output data connected to the LEDs (low active); GPIOL_21,GPIOL_18,GPIOL_16,GPIOL_09,GPIOL_03
+	end component;
 
 	SIGNAL clk :  std_logic := '0';
-	SIGNAL setn :  std_logic:= '1';
-	SIGNAL stopn :  std_logic:= '1';
+	SIGNAL setn :  std_logic := '1';
+	SIGNAL stopn :  std_logic := '1';
 	SIGNAL Dataout :  std_logic_vector(3 downto 0);
-	SIGNAL PERIOD : time := 30ns;
-	SIGNAL PERIOD2 : time := PERIOD/2;
+	constant PERIOD : time := 20 ns;
+	constant HALF_PERIOD : time := PERIOD/2;
 
 BEGIN
 
@@ -33,26 +33,27 @@ BEGIN
 	);
 
 -- clock generation
-clk <= not clk after PERIOD2;
+clk <= not clk after HALF_PERIOD;
+
 -- *** Test Bench - User Defined Section ***
    tb : PROCESS
    BEGIN
-     setn <='1';
-	 stopn <= '1';
-	 wait for PERIOD *10;
-	 setn <='1';
-	 stopn <= '0'; 
-	 wait for PERIOD *5;
-	 setn <='1';
-	 stopn <= '1'; 	
+    setn <='1';
+	stopn <= '1';
+	wait for PERIOD *24;
+	setn <='1';
+	stopn <= '0'; 
+	wait for PERIOD *12;
+	setn <='1';
+	stopn <= '1'; 	
 	wait for PERIOD *5;
-	 setn <='0';
-	 stopn <= '1';  
+	setn <='0';
+	stopn <= '1';  
 	wait for PERIOD *5;
-	 setn <='1';
-	 stopn <= '1'; 
-      wait; -- will wait forever
-   END PROCESS;
+	setn <='1';
+	stopn <= '1'; 
+    assert false report "Testbench ran until end" severity failure;
+   END PROCESS tb;
 -- *** End Test Bench - User Defined Section ***
 
 END;
